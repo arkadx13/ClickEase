dec2a4b4088f9537a3283ddf8402bc9a1f48dee5
Mariel Juliene Macabudbud <arkadx213@gmail.com>
2024-05-09 17:43:40 +0800

    trimmed down loading cards to removed scroll bar

--- src/components/ShimmerHome.js
+++ src/components/ShimmerHome.js
@@ -8,7 +8,7 @@ const ShimmerHome = () => {
 	}, []);
 
 	return (
-		<div className="d-flex flex-row overflow-auto">
+		<div className="d-flex flex-row justify-content-center">
 			<Card
 				className="m-2"
 				style={{
@@ -25,111 +25,6 @@ const ShimmerHome = () => {
 					</Placeholder>
 					<Placeholder as={Card.Text} animation="glow">
 						<Placeholder xs={7} /> <Placeholder xs={4} />{" "}
-						<Placeholder xs={8} />
-					</Placeholder>
-				</Card.Body>
-			</Card>
-			<Card
-				className="m-2"
-				style={{
-					height: "auto",
-					minWidth: "200px",
-					width: "200px",
-					maxWidth: "250px",
-				}}
-			>
-				<Card.Img variant="top" src={PlaceholderImage} />
-				<Card.Body>
-					<Placeholder as={Card.Title} animation="glow">
-						<Placeholder xs={6} />
-					</Placeholder>
-					<Placeholder as={Card.Text} animation="glow">
-						<Placeholder xs={7} /> <Placeholder xs={4} />{" "}
-						<Placeholder xs={4} /> <Placeholder xs={6} />{" "}
-						<Placeholder xs={8} />
-					</Placeholder>
-				</Card.Body>
-			</Card>
-			<Card
-				className="m-2"
-				style={{
-					height: "auto",
-					minWidth: "200px",
-					width: "200px",
-					maxWidth: "250px",
-				}}
-			>
-				<Card.Img variant="top" src={PlaceholderImage} />
-				<Card.Body>
-					<Placeholder as={Card.Title} animation="glow">
-						<Placeholder xs={6} />
-					</Placeholder>
-					<Placeholder as={Card.Text} animation="glow">
-						<Placeholder xs={7} /> <Placeholder xs={4} />{" "}
-						<Placeholder xs={4} /> <Placeholder xs={6} />{" "}
-						<Placeholder xs={8} />
-					</Placeholder>
-				</Card.Body>
-			</Card>
-			<Card
-				className="m-2"
-				style={{
-					height: "auto",
-					minWidth: "200px",
-					width: "200px",
-					maxWidth: "250px",
-				}}
-			>
-				<Card.Img variant="top" src={PlaceholderImage} />
-				<Card.Body>
-					<Placeholder as={Card.Title} animation="glow">
-						<Placeholder xs={6} />
-					</Placeholder>
-					<Placeholder as={Card.Text} animation="glow">
-						<Placeholder xs={7} /> <Placeholder xs={4} />{" "}
-						<Placeholder xs={4} /> <Placeholder xs={6} />{" "}
-						<Placeholder xs={8} />
-					</Placeholder>
-				</Card.Body>
-			</Card>
-			<Card
-				className="m-2"
-				style={{
-					height: "auto",
-					minWidth: "200px",
-					width: "200px",
-					maxWidth: "250px",
-				}}
-			>
-				<Card.Img variant="top" src={PlaceholderImage} />
-				<Card.Body>
-					<Placeholder as={Card.Title} animation="glow">
-						<Placeholder xs={6} />
-					</Placeholder>
-					<Placeholder as={Card.Text} animation="glow">
-						<Placeholder xs={7} /> <Placeholder xs={4} />{" "}
-						<Placeholder xs={4} /> <Placeholder xs={6} />{" "}
-						<Placeholder xs={8} />
-					</Placeholder>
-				</Card.Body>
-			</Card>
-			<Card
-				className="m-2"
-				style={{
-					height: "auto",
-					minWidth: "200px",
-					width: "200px",
-					maxWidth: "250px",
-				}}
-			>
-				<Card.Img variant="top" src={PlaceholderImage} />
-				<Card.Body>
-					<Placeholder as={Card.Title} animation="glow">
-						<Placeholder xs={6} />
-					</Placeholder>
-					<Placeholder as={Card.Text} animation="glow">
-						<Placeholder xs={7} /> <Placeholder xs={4} />{" "}
-						<Placeholder xs={4} /> <Placeholder xs={6} />{" "}
 						<Placeholder xs={8} />
 					</Placeholder>
 				</Card.Body>
